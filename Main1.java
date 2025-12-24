45e7f91e8b0e70075ef0a4efa92185fddc3157ba
paco <pacomamaema@gmail.com>
2025-12-24 14:57:18 +0100

    Problema2 añadido

---
+++ Main1.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static void main(String)[] args) {System.out.println("Let´s play a fun game :3");
+    public static void main(String)[] {System.out.println("If you guess the correct random number from 1 to 3, you win!;");
+
+
+
